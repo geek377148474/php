bbddd3fefb8ef174b9d4529e4ccf3c0403f4aea1
ccjian <377148474@qq.com>
2019-07-02 18:37:50 +0800

    修改foreach/*

--- foreach/index.php
+++ foreach/index.php
@@ -2,5 +2,14 @@
 require 'common/function.php';
 
 # [foreach的迭代引用] 每次遍历相当于 $v = $arr[$key]
-
+/*
+foreach每次操作一个单元，
+都是将其索引和值分别取到变量中，
+或者只取出值到一个变量中，
+然后单独操作放有索引和值的变量，
+不会影响到被遍历的对象本身。
+如果要在遍历过程中修改对象中的值，
+需要在声明是在变量前加&符号。
+例如：foreach($array as &$value）。
+*/
 require 'foreach/example.php';
