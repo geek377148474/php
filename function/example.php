c6828792ec906b228eef6a72fe7a9c8bbe680fb7
ccjian <377148474@qq.com>
2019-07-16 15:12:29 +0800

    添加 function/getArgsAsArray

---
+++ function/example.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 将选定的后半段输入的参数作为一个集合array
+ *
+ * @param [type] $abstracts
+ * @param [type] $tags
+ * @return void
+ */
+function getArgsAsArray($abstracts, $tags)
+{
+    echo 'this is a '.$abstracts.PHP_EOL;
+    $tags = is_array($tags) ? $tags : array_slice(func_get_args(), 1);
+    return $tags;
+}
+
+$args = getArgsAsArray('test', 'arg1', 'arg2', 'arg3');
+
+dump($args);
--- xhprof/index.php
+++ xhprof/index.php
@@ -1,6 +1,6 @@
 <?php
 require 'common/function.php';
-$dir = 'function';// 工作目录
+$dir = 'xhprof';// 工作目录
 if (!empty($dir) && dirname($_SERVER['SCRIPT_FILENAME']) != $dir) {
     chdir($dir);
 }
