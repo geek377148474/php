d86735adb0e895120002fe1c5887ef46d7a3d27e
ccjian <377148474@qq.com>
2019-09-12 09:38:39 +0800

    Php-validate

--- Php-validate/1.php
+++ Php-validate/1.php
@@ -2,15 +2,17 @@
 
 use Inhere\Validate\Validation;
 
+// https://github.com/inhere/php-validate
+
 $crossover = [
-    'message' => 'hello world!',
+    'message' => 'hello world',
     'code' => 666666,
     'data' => 'ok.'
 ];
 
 $v = Validation::check($crossover,[
     // add rule
-    ['message', 'min', 12], // 最短得多少
+    ['message', 'min', 12, "msg" => "{attr} error msg!"], // 最短得多少
     ['code', 'number'], // 类型
     ['code', 'required'],
     ['data', 'required'],
